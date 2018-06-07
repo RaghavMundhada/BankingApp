da5c9f05c8cb3e97e7820c2dc10fc524e1f46f98
raghav.m <raghav.m@directi.com>
2018-06-08 00:47:18 +0530

    User package done.

--- src/main/java/bankingApp/user/customer/Customer.java
+++ src/main/java/bankingApp/user/customer/Customer.java
@@ -6,11 +6,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-    private String customerId;
     private String userId;
-    private String password;
+    private String name;
+    private String emailId;
+    private String mobileNumber;
 
-    private List<AbstractAccount> account = new ArrayList<AbstractAccount>();
+    public Customer(String userId, String name, String emailId, String mobileNumber) {
+        this.userId = userId;
+        this.name = name;
+        this.emailId = emailId;
+        this.mobileNumber = mobileNumber;
+    }
 
+    public String getUserId() {
+        return userId;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
 }
--- src/main/java/bankingApp/user/employee/Employee.java
+++ src/main/java/bankingApp/user/employee/Employee.java
@@ -1,4 +1,39 @@
 package bankingApp.user.employee;
 
-public class Employee {
+import bankingApp.user.customer.Customer;
+import com.sun.deploy.util.StringUtils;
+
+import java.util.regex.Pattern;
+
+public class Employee extends Customer{
+    public Employee(String userId, String name, String emailId, String mobileNumber) {
+        super(userId, name, emailId, mobileNumber);
+    }
+
+    public static boolean isDetailsValid(Customer customer){
+        if(!isValidEmail(customer.getEmailId())){
+            return false;
+        }
+
+        if(customer.getName().isEmpty()){
+            return false;
+        }
+
+        if(!customer.getMobileNumber().matches("-?\\d+(\\.\\d+)?")){
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isValidEmail(String emailId) {
+        String emailReg = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern p = Pattern.compile(emailReg);
+
+        return p.matcher(emailId).matches();
+    }
 }
